80e01e70ae4e1da9eb614aee18f1b5d3986b2294
anfgk <gloria020510@gmail.com>
2025-07-04 01:45:06 +0900

    WebSocket Messages

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,21 @@
 const socket = new WebSocket(`ws://${window.location.host}`); // 웹 클라이언트(브라우저)에서 WebSocket 서버에 연결할 때 사용
+
+// 웹소켓 연결이 성공해서 열렸을 때 실행되는 함수
+socket.addEventListener("open", () => {
+    console.log("Connected to Server ⭕️"); 
+});
+
+// 서버로부터 메시지를 받았을 때 실행되는 함수
+socket.addEventListener("message", (message) => {
+    console.log("New message: ", message.data);
+})
+
+// 서버와의 연결이 끊겼을 때 실행되는 함수
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌"); 
+});
+
+// 10초(10000밀리초)후에 실행되는 코드
+setTimeout(() => {
+    socket.send("hello from the browser"); // 서버에게 "hello from the browser" 메시지를 보냄
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -10,8 +10,7 @@ const app = express(); // 앱 만들기
 // 👉🏻 express()는 웹 서버 만들기 위한 기본 뼈대
 // 👉🏻 웹 서버 역할을 하는 Express 애플리케이션 객체
 
-app.set("view engine", "pug"); // pug를 템플릿 엔진으로 사용
-// 엔진: HTML을 자동으로 만들어주는 도구
+app.set("view engine", "pug"); // pug를 템플릿 엔진으로 사용 // 엔진: HTML을 자동으로 만들어주는 도구
 app.set("views", __dirname + "/views"); // 템플릿 파일들이 있는 폴더 설정
 app.use("/public", express.static(__dirname + "/public")); // 정적 파일(css, 이미지 등) 제공
 app.get("/", (_, res) => res.render("home")); // 홈(/)에 오면 home.pug 보여주기
@@ -24,11 +23,23 @@ const server = http.createServer(app); // 서버 만들기
 const wss = new WebSocket.Server({ server }); // http 서버,WebSocket 서버 같이 구동
 
 // WebSocket 연결이 새로 생겼을 때 실행되는 콜백 함수
-function handleConnection(socket) {
-    console.log(socket);
-}
+// function handleConnection(socket) {
+//     console.log(socket);
+// }
 
-wss.on("connection", handleConnection); 
+// wss.on("connection", handleConnection); 
 // WebSocket 서버(wss)가 클라이언트와 연결될 때마다 handleConnection 함수를 호출하겠다는 의미
+
+// 누군가 서버에 웹소켓으로 연결하면 실행됨
+wss.on("connection", (socket) => {
+//   console.log(socket); // 연결된 사람 정보를 콘솔에 출력
+    console.log("Connected to Browser ✅"); 
+    socket.on("close", () => console.log("Disconnected from the Browser ❌")); // 브라우저 쪽에서 연결을 끊었을 때 실행됨
+    // 클라이언트가 서버로 메시지를 보내면 실행되는 함수
+    socket.on("message", (message) => {
+    console.log(message.toString()); // 받은 메시지를 콘솔에 출력
+    }); 
+    socket.send("hello!!!"); // 연결된 사람에게 메시지 보내기
+});
 
 server.listen(3000, handleListen); // 3000번 포트에서 서버 실행
