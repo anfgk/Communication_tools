92bd9835fd8e223b0310f689ff46f294f13eeb76
anfgk <gloria020510@gmail.com>
2025-07-03 23:55:05 +0900

    WebSockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -8,8 +8,8 @@ app.set("view engine", "pug"); // pug를 템플릿 엔진으로 사용
 // 엔진: HTML을 자동으로 만들어주는 도구
 app.set("views", __dirname + "/views"); // 템플릿 파일들이 있는 폴더 설정
 app.use("/public", express.static(__dirname + "/public")); // 정적 파일(css, 이미지 등) 제공
-app.get("/", (req, res) => res.render("home")); // 홈(/)에 오면 home.pug 보여주기
-app.get("/*", (req, res) => res.redirect("/")); // 어떤 주소로 들어와도 홈 페이지로 보내기
+app.get("/", (_, res) => res.render("home")); // 홈(/)에 오면 home.pug 보여주기
+app.get("/*", (_, res) => res.redirect("/")); // 어떤 주소로 들어와도 홈 페이지로 보내기
 
-const handleListening = () => console.log(`Server listening on port http://localhost:3000`);
+const handleListening = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListening); // 3000번 포트에서 서버 실행
